bbe3035e3c2d9aa5f0b89db9292174bd90c6d42c
yordandev <19682540+yordandev@users.noreply.github.com>
2021-04-08 14:13:00 +0200

    start frontend

---
+++ frontend/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,500;1,500&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" href="./img/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="./css/layout.css">
+    <link rel="stylesheet" type="text/css" href="./css/index.css">
+    <title>Wisteria E-Commerce</title>
+</head>
+
+<body>
+    <h1>bye</h1>
+</body>
+
+</html>
