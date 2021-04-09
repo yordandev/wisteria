d14fba2607ee9cd78310503aa01c9293b4c6ddff
agnes <johanssonagnes@icloud.com>
2021-04-09 11:09:27 +0200

    login, signup and authentication

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,38 @@
 const db = require("./db.js");
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 3000;
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+const { request } = require("express");
+
+const generateAccessToken = (data) =>
+  //expires after an hour
+  jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "3600s" });
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  //if not empty split
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) {
+    res.status(401).json({
+      error: "You need to be authenticated",
+    });
+  }
+
+  jwt.verify(token, JWT_SECRET, async (err, user) => {
+    if (err) {
+      res.status(401).json({
+        error: "You need to be authenticated",
+      });
+    }
+    request.token = token;
+    next();
+  });
+};
 
 app.use(bodyParser.json());
 
@@ -58,21 +86,7 @@ app.post("/products", (req, res) => {
   );
 });
 
-app.get("/users", (req, res) => {
-  db.query(
-    "SELECT id, email, userType FROM user",
-    function (error, results, fields) {
-      if (error) {
-        console.log(error);
-      }
-      // error will be an Error if one occurred during the query
-      // results will contain the results of the query
-      // fields will contain information about the returned results fields (if any)
-      res.json(results);
-    }
-  );
-});
-
+//registering only if email is unique NOT FINISHED
 app.post("/signup", async (req, res) => {
   const myPlaintextPassword = req.body.password;
   console.log(myPlaintextPassword);
@@ -87,13 +101,11 @@ app.post("/signup", async (req, res) => {
         console.log(error);
       }
       numRows = results.length;
-      console.log(numRows);
       if (numRows == 1) {
         res.status(409).json({
-          message: "This email is already registered.",
+          error: "This email is already registered.",
         });
       } else {
-        console.log("insert");
         db.query(
           "INSERT INTO user(email, password) VALUES (?, ?)",
           values,
@@ -101,14 +113,76 @@ app.post("/signup", async (req, res) => {
             if (error) {
               console.log(error);
             }
+            const tokenData = {
+              email: req.body.email,
+              id: results.insertId,
+            };
+            const genToken = generateAccessToken(tokenData);
             res.status(201).json({
               message: "You successfully registered!",
               id: results.insertId,
               email: req.body.email,
+              token: genToken,
             });
           }
         );
       }
+    }
+  );
+});
+
+app.post("/login", async (req, res, next) => {
+  const passwordSql = "SELECT password FROM user WHERE email = ?";
+  const passwordSqlValue = req.body.email;
+  const data = await db.query(
+    passwordSql,
+    passwordSqlValue,
+    function (error, results, fields) {
+      if (!results.length) {
+        res.status(400).json({
+          error: "Invalid email or the email is not registered!",
+        });
+      }
+      const storedHashValue = results[0].password;
+      if (bcrypt.compareSync(req.body.password, storedHashValue)) {
+        db.query(
+          "SELECT id, email, userType FROM user WHERE password = ?",
+          storedHashValue,
+          function (error, results, fields) {
+            if (error) {
+              console.log(error);
+            }
+            const tokenData = {
+              email: results.email,
+              id: results.id,
+            };
+            const genToken = generateAccessToken(tokenData);
+            res.status(200).json({
+              message: "You are now logged in!",
+              user: results,
+              token: genToken,
+            });
+          }
+        );
+      } else {
+        res.status(401).json({
+          error: "Invalid password!",
+        });
+      }
+    }
+  );
+});
+
+//users endpoint, getting all users NOT FINISHED
+app.get("/users", (req, res) => {
+  db.query(
+    "SELECT id, email, userType FROM user",
+
+    function (error, results, fields) {
+      if (error) {
+        console.log(error);
+      }
+      res.json(results);
     }
   );
 });
