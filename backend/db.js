9c868c09520355c3feb331e457cdbe8a69e4d302
yordandev <19682540+yordandev@users.noreply.github.com>
2021-04-14 19:49:08 +0200

    fix cascade delete, typechecking, userType check

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,18 @@
-require("dotenv").config();
-const mysql = require("mysql");
+require('dotenv').config()
+const mysql = require('mysql')
+
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+})
 
 connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+	if (err) {
+		console.error('Error connecting to the database: ' + err.stack)
+		return
+	}
+})
 
-  console.log("connected as id " + connection.threadId);
-});
-
-module.exports = connection;
+module.exports = connection
