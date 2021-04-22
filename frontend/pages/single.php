617a47f092e3e8cdbf0ce022e35ec79930dbbaaa
agnes <johanssonagnes@icloud.com>
2021-04-22 18:03:22 +0200

    added single product page

---
+++ frontend/pages/single.php
@@ -0,0 +1,15 @@
+<div id="singleProductPage">
+    <div id="product">
+        <figure>
+            <img src="https://picsum.photos/100/200" alt="Item 1">
+        </figure>
+        <div class="productInfo">
+            <h3>Brand name</h3>
+            <h4>Product title</h4>
+            <h4>Fit and condition</h4>
+            <h4>Size</h4>
+            <h4>Price</h4>
+            <button>Add to cart</button>
+        </div>
+    </div>
+</div>
