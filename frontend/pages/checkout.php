87389b30ff7be6871328330ee9df4ddb2e156a5f
yordandev <19682540+yordandev@users.noreply.github.com>
2021-05-03 10:48:12 +0200

    home img

--- frontend/pages/checkout.php
+++ frontend/pages/checkout.php
@@ -24,51 +24,9 @@
     <div class="cart">
         <h1>Cart (<?php print(count($cartItems)) ?>)</h1>
         <ul>
-            <li>
-                <div>
-                    <figure>
-                        <img src="https://picsum.photos/100/200" alt="Item 1">
-                    </figure>
-                    <div>
-                        <h3>Brand name</h3>
-                        <h4>Product title</h4>
-                        <h4>Fit and condition</h4>
-                        <h4>Size</h4>
-                        <h4>Price</h4>
-                    </div>
-                    <a href="#">Remove</a>
-                </div>
-            </li>
-            <li>
-                <div>
-                    <figure>
-                        <img src="https://picsum.photos/100/200" alt="Item 1">
-                    </figure>
-                    <div>
-                        <h3>Brand name</h3>
-                        <h4>Product title</h4>
-                        <h4>Fit and condition</h4>
-                        <h4>Size</h4>
-                        <h4>Price</h4>
-                    </div>
-                    <a href="#">Remove</a>
-                </div>
-            </li>
-            <li>
-                <div>
-                    <figure>
-                        <img src="https://picsum.photos/100/200" alt="Item 1">
-                    </figure>
-                    <div>
-                        <h3>Brand name</h3>
-                        <h4>Product title</h4>
-                        <h4>Fit and condition</h4>
-                        <h4>Size</h4>
-                        <h4>Price</h4>
-                    </div>
-                    <a href="#">Remove</a>
-                </div>
-            </li>
+            <?php echoCard();
+            echoCard();
+            echoCard(); ?>
         </ul>
         <div class="total">
             <p>Total</p>
--- frontend/pages/home.php
+++ frontend/pages/home.php
@@ -1,7 +1,28 @@
 <div>
-    <figure id="inspoImg1" class="inspoImg"><img src="https://picsum.photos/800/500" /></figure>
-    <figure id="inspoImg2" class="inspoImg"><img src="https://picsum.photos/600/500" /></figure>
-    <figure id="inspoImg3" class="inspoImg"><img src="https://picsum.photos/1000/400" /></figure>
+    <a href="">
+        <figure id="inspoImg1" class="inspoImg">
+            <img src="https://picsum.photos/800/500" />
+            <div class="overlay">
+                <div class="text">SHOP MEN</div>
+            </div>
+        </figure>
+    </a>
+    <a href="">
+        <figure id="inspoImg2" class="inspoImg">
+            <img src="https://picsum.photos/600/500" />
+            <div class="overlay">
+                <div class="text">SHOP WOMEN</div>
+            </div>
+        </figure>
+    </a>
+    <a href="">
+        <figure id="inspoImg3" class="inspoImg">
+            <img src="https://picsum.photos/1000/400" />
+            <div class="overlay">
+                <div class="text">SHOP UNISEX</div>
+            </div>
+        </figure>
+    </a>
 </div>
 <h2>New Arrivals</h2>
-<?php echo echoGrid(8)?>
+<?php echo echoGrid(8) ?>
