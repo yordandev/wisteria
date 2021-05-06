96f4612bc4afeb1daf9611644b11933f0be228bd
Antonia Ziegler <97anzier@gmail.com>
2021-05-06 12:27:59 +0200

    static pages

---
+++ frontend/pages/about.php
@@ -0,0 +1,5 @@
+<h1>About Us</h1>
+<p>Hello there!<br>
+    We started this company with our love for fashion and to reduce peoples' impact on the environment!
+</p>
+<figure><img src="../img/team.jpg"></figure>
---
+++ frontend/pages/privacyPolicy.php
@@ -0,0 +1,2 @@
+<h1>Privacy Policy</h1>
+<p>We do not give out any of your information to a third party</p>
