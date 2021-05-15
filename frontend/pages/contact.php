2bac71bd29187a4137526cbacf33b4731a3918f2
Antonia Ziegler <97anzier@gmail.com>
2021-05-15 17:31:02 +0200

    fix

---
+++ frontend/pages/contact.php
@@ -0,0 +1,23 @@
+<h1>Contact</h1>
+<p><b>
+        Phone number:
+    </b>
+    <br>
+    <a href="tel:#">
+        xxx xxx xx xx
+    </a>
+</p>
+<p><b>
+        Email:
+    </b>
+    <br>
+    <a href="mailto:#">
+        webmaster@example.com
+    </a>
+</p>
+<p><b>
+        Office adress:
+    </b>
+    <br>
+    Gjuterigatan 5, 553 18 Jönköping
+</p>
