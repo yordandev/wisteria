f8eafb6721652a2b93d240e221dbd5a8120daed0
yordandev <19682540+yordandev@users.noreply.github.com>
2021-05-21 12:16:16 +0200

    remove phpsessid on logout

--- frontend/public/pages/logout.php
+++ frontend/public/pages/logout.php
@@ -1,8 +1,7 @@
 <?php
-$params = session_get_cookie_params();
-setcookie(session_name(), '', 0, $params['path'], $params['domain'], $params['secure'], isset($params['httponly']));
 session_unset();
 session_destroy();
+setcookie("PHPSESSID", "", time() - 1, '/');
 
 echo "<script>window.location.href = '/'</script>";
 exit;
